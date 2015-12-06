82d157f81c39be39b6631b860ba0c04b6d12c294
Felix Jaeckel <jaeckel2@wisc.edu>
2015-12-05 22:00:30 -0600

    Add resistance to subscriber, create minimal demo

--- Zmq/Subscribers.py
+++ Zmq/Subscribers.py
@@ -6,15 +6,36 @@ Created on Thu Aug 27 14:16:57 2015
 """
 
 from Zmq import ZmqSubscriber
+from Ports import PubSub
 from PyQt4.QtCore import pyqtSignal
 
 class TemperatureSubscriber(ZmqSubscriber):
     adrTemperatureReceived = pyqtSignal(float)
+    adrResistanceReceived = pyqtSignal(float)
     def __init__(self, parent=None):
-        ZmqSubscriber.__init__(self, port=5555, parent=parent)
+        ZmqSubscriber.__init__(self, port=PubSub.AdrTemperature, parent=parent)
         self.floatReceived.connect(self.processFloat)
         self.maxAge = 3
 
     def processFloat(self, origin, item, value, timeStamp):
         if item == u'ADR_Temperature':
             self.adrTemperatureReceived.emit(value)
+        elif item == u'ADR_Sensor_R':
+            self.adrResistanceReceived.emit(value)
+
+
+if __name__ == '__main__':
+    from PyQt4.QtGui import QApplication, QDoubleSpinBox
+    app = QApplication([])
+    widget = QDoubleSpinBox()
+    widget.setDecimals(7)
+    widget.setMinimum(0)
+    widget.setMaximum(1000)
+    widget.setSuffix(' K')
+    widget.setReadOnly(True)
+    sub = TemperatureSubscriber(widget)
+    sub.adrTemperatureReceived.connect(widget.setValue)
+    widget.show()
+    sub.start()
+    app.exec_()
+    
