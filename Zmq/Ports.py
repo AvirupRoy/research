a68b4e12c78a19b0755f9d3cf86b8eba916ab17e
Felix Jaeckel <jaeckel2@wisc.edu>
2015-12-05 22:00:59 -0600

    List the ZMQ ports in a common file

---
+++ Zmq/Ports.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 27 16:54:17 2015
+
+@author: wisp10
+"""
+
+class PubSub:
+    AdrTemperature = 5555
+    MagnetControl = 5557
+    
+class RequestReply:
+    MagnetControl = 5558
+    OpenSquid = 5560
+    AdrPidControl = 5562
