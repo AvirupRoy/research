53e12f0bdf657a127911f6a6285be6e203ee8a9b
Felix Jaeckel <jaeckel2@wisc.edu>
2016-06-30 18:31:04 -0500

    Add more "universal" get and set value function for widgets

--- LabWidgets/Utilities.py
+++ LabWidgets/Utilities.py
@@ -33,26 +33,54 @@ def connectAndUpdate(widget, slot):
     else:
         raise Exception('connectAndUpdate cannot handle %s' % type(widget))
 
-def saveWidgetToSettings(settings, widget):
-    name = widget.objectName()
-    s = settings
+def widgetValue(widget):
+    '''A clever little function to obtain the "value" of a widget for any widget of typical types'''
     if isinstance(widget, QtGui.QAbstractSlider) or isinstance(widget, QtGui.QAbstractSpinBox):
-        s.setValue(name, widget.value())
+        return widget.value()
     elif isinstance(widget, QtGui.QPlainTextEdit):
-        s.setValue(name, widget.toPlainText())
+        return widget.toPlainText()
     elif isinstance(widget, QtGui.QTextEdit) or isinstance(widget, QtGui.QLineEdit):
-        s.setValue(name, widget.text())
+        return widget.text()
     elif isinstance(widget, QtGui.QComboBox):
-        s.setValue(name, widget.currentText())
+        return widget.currentText()
     elif isinstance(widget, QtGui.QAbstractButton):
-        s.setValue(name, widget.isChecked())
+        return widget.isChecked()
     elif isinstance(widget, QtGui.QGroupBox):
-        s.setValue(name, widget.isChecked())
+        return widget.isChecked()
+    else:
+        print"Can't deal with %s" % widget
+    
+def saveWidgetToSettings(settings, widget, name = None):
+    '''Save a widget's value to QSettings. If name is not specified,
+    the widgets objectName() will be used as the default key.'''
+    if name is None:
+        name = widget.objectName()
+    settings.setValue(name, widgetValue(widget))
+    
+def setWidgetValue(widget, value):
+    if isinstance(widget, QtGui.QAbstractSlider) or isinstance(widget, QtGui.QSpinBox):
+        widget.setValue( value )
+    elif isinstance(widget, QtGui.QDoubleSpinBox):
+        widget.setValue( value )
+    elif isinstance(widget, QtGui.QPlainTextEdit):
+        widget.setPlainText( value )
+    elif isinstance(widget, QtGui.QTextEdit) or isinstance(widget, QtGui.QLineEdit):
+        widget.setText( value )
+    elif isinstance(widget, QtGui.QComboBox):
+        i = widget.findText( value )
+        widget.setCurrentIndex(i)
+    elif isinstance(widget, QtGui.QAbstractButton):
+        widget.setChecked( value )
+    elif isinstance(widget, QtGui.QGroupBox):
+        widget.setChecked( value )
     else:
         print"Can't deal with %s" % widget
 
-def restoreWidgetFromSettings(settings, widget):
-    name = widget.objectName()
+def restoreWidgetFromSettings(settings, widget, name = None):
+    '''Restore a widget's value from QSettings. If name is not specified,
+    the widgets objectName() will be used as the default key.'''
+    if name is None:
+        name = widget.objectName()
     s = settings
     if isinstance(widget, QtGui.QAbstractSlider) or isinstance(widget, QtGui.QSpinBox):
         widget.setValue( s.value(name, widget.value(), type=int) )
