d585f40353e01f64f614419ed458e75754ea727f
Felix Jaeckel <jaeckel2@wisc.edu>
2017-11-21 17:47:36 -0600

    Implement ZMQ aggregator/forwarder for housekeeping

---
+++ Zmq/ZmqForward.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+A simple forwarder to aggregate data from other publishers into a single publisher
+
+Created on Tue Nov 21 14:34:29 2017
+@author: Felix Jaeckel <felix.jaeckel@wisc.edu>
+"""
+
+import zmq
+from Ports import PubSub
+import logging
+
+try:
+    context = zmq.Context()
+    frontEnd = context.socket(zmq.SUB)
+    frontEnd.setsockopt(zmq.SUBSCRIBE, '')
+    ports = [PubSub.AdrTemperature, PubSub.LockinThermometerAdr, PubSub.LockinThermometerBox, PubSub.LockinThermometerRuOx2005]
+    for port in ports:
+        frontEnd.connect('tcp://127.0.0.1:%d' % port)
+        
+    outputPort = PubSub.Housekeeping
+    backEnd = context.socket(zmq.PUB)
+    backEnd.bind('tcp://*:%d' % outputPort)
+    
+    zmq.device(zmq.FORWARDER, frontEnd, backEnd)
+except Exception as e:
+    logging.error(e, exc_info=True)
+finally:
+    frontEnd.close()
+    backEnd.close()
+    context.term()
