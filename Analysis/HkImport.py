47d154d2fc6d497015e11a741b27892aaac56c60
Felix Jaeckel <jaeckel2@wisc.edu>
2017-12-01 12:29:26 -0600

    Add housekeeping import classes: HkImporter

---
+++ Analysis/HkImport.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Code to read the housekeeping data from HDF5 files as produced by Utility/HkLogger.py
+Created on Thu Nov 30 16:50:12 2017
+
+@author: wisp10
+"""
+
+class HkThermometer(object):
+    def __init__(self, hdfRoot):
+        self.sensorName = hdfRoot.attrs['SensorName']
+        self.t = hdfRoot['t'].value
+        self.P = hdfRoot['P'].value
+        self.R = hdfRoot['R'].value
+        self.T = hdfRoot['T'].value
+        
+    def plotT(self):
+        import matplotlib.pyplot as mpl
+        mpl.plot(self.t, self.T, '-')
+        
+class HkTesBias(object):
+    def __init__(self, hdfRoot):
+        self.t = hdfRoot['t'].value
+        self.Vbias = hdfRoot['Vbias'].value
+
+class HkFieldCoil(object):
+    def __init__(self, hdfRoot):
+        self.t = hdfRoot['t'].value
+        self.Vcoil = hdfRoot['Vcoil'].value
+        
+class HkMagnet(object):
+    def __init__(self, hdfRoot):
+        self.t = hdfRoot['t']
+        self.Icoarse = hdfRoot['ImagnetCoarse'].value
+        self.Ifine = hdfRoot['ImagnetFine'].value
+        self.Vmagnet = hdfRoot['Vmagnet'].value
+
+class HkImporter(object):
+    def __init__(self, hdfRoot):
+        thermometers = {}
+        for key in hdfRoot.keys():
+            if 'Thermometer' in key or key in ['GGG', 'FAA']:
+                thermometers[key] = HkThermometer(hdfRoot[key])
+        self.thermometers = thermometers
+        self.magnet = HkMagnet(hdfRoot['Magnet'])
+        self.tesBias = HkTesBias(hdfRoot['TESBias'])
+        self.fieldCoil = HkFieldCoil(hdfRoot['FieldCoilBias'])
